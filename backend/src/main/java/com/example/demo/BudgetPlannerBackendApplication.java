4cb2d89681724c8bebf23d628db8e32696c25daf
2300031714-GrandhiRiteshsai <163728488+2300031714-GrandhiRiteshsai@users.noreply.github.com>
2025-09-26 21:52:29 +0530

    Update BudgetPlannerBackendApplication.java

--- backend/src/main/java/com/example/demo/BudgetPlannerBackendApplication.java
+++ backend/src/main/java/com/example/demo/BudgetPlannerBackendApplication.java
@@ -1,23 +1,23 @@
-package com.example.demo;
+package com.budgetplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.budgetplannerbackend.repository")
-@EntityScan("com.budgetplannerbackend.model")
-@SpringBootApplication(scanBasePackages = "com.budgetplannerbackend")
+@SpringBootApplication(scanBasePackages = "com.budgetplanner")
+@EnableJpaRepositories("com.budgetplanner.repository")
+@EntityScan("com.budgetplanner.model")
 public class BudgetPlannerBackendApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetPlannerBackendApplication.class, args);
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BudgetPlannerBackendApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(BudgetPlannerBackendApplication.class, args);
     }
 }
