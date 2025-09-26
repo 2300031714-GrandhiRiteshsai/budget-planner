569128f3bca5190831d0d2e22e10b1f495f85e0b
2300031714-GrandhiRiteshsai <163728488+2300031714-GrandhiRiteshsai@users.noreply.github.com>
2025-09-26 22:17:14 +0530

    Update BudgetPlannerBackendApplication.java

--- backend/src/main/java/com/example/demo/BudgetPlannerBackendApplication.java
+++ backend/src/main/java/com/example/demo/BudgetPlannerBackendApplication.java
@@ -4,20 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.budgetplanner")
 @EnableJpaRepositories("com.budgetplanner.repository")
 @EntityScan("com.budgetplanner.model")
 public class BudgetPlannerBackendApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BudgetPlannerBackendApplication.class, args);
-    }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BudgetPlannerBackendApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetPlannerBackendApplication.class, args);
     }
 }
